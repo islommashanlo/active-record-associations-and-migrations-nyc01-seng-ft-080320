64c71672cf739c62155d6c6a3d9234e09b3fd468
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-17 03:44:59 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,8 +2,6 @@ class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table :artists do |t|
       t.string :name
-      t.string :song
-      t.string :genre
     end
   end
 end
